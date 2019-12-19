8758367627f7c37332725cbf388b3c53466d153f
Konstantin Romanov <kromanov1@bloomberg.net>
2019-12-19 18:01:43 +0000

    Disable trmc optimization for stored_ident (fixes segfault in ocamlopt.opt).

--- lambda/translmod.ml
+++ lambda/translmod.ml
@@ -1149,8 +1149,9 @@ let transl_store_structure glob map prims aliases str =
               | [] ->
                 transl_store rootpath (add_idents true ids subst) cont rem
               | id :: idl ->
+                  let stored_ident = store_ident loc id in
                   Llet(Alias, Pgenval, id, Lprim(Pfield pos, [Lvar mid], loc),
-                       Lsequence(store_ident loc id,
+                       Lsequence(stored_ident,
                                  store_idents (pos + 1) idl))
             in
             Llet(Strict, Pgenval, mid,
